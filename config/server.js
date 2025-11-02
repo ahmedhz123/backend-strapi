99abde666d952a436f09ce6f92f973cab80940c6
ahmedhz123 <ahmedhamzazagger123@gmail.com>
2025-11-02 20:54:44 +0200

    fix host and the port

--- config/server.js
+++ config/server.js
@@ -1,9 +1,10 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 8080),
   app: {
     keys: env.array('APP_KEYS'),
   },
+   url: env('PUBLIC_URL', 'https://your-app-name.up.railway.app'),
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
