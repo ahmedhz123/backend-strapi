c8a5f34deb7b06eaedfc1af41b4262be347b1b30
ahmedhz123 <ahmedhamzazagger123@gmail.com>
2025-11-03 12:50:48 +0200

    new port

--- config/server.js
+++ config/server.js
@@ -1,10 +1,10 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 8080),
+  port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
   },
-   url: env('PUBLIC_URL', 'https://your-app-name.up.railway.app'),
+   url: env('PUBLIC_URL', 'https://backend-strapi.up.railway.app'),
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
