4948f72fc485ef7eb49e5ff2a55e9498be75f111
ahmedhz123 <ahmedhamzazagger123@gmail.com>
2025-11-04 14:10:35 +0200

    v

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,7 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+  'users-permissions': {
+    config: {
+      jwtSecret: env('JWT_SECRET', 'BA6/LPDxi1g37/KpZXcJNQ=='),
+    },
+  },
+});
