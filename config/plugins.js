db912e1feaef745ca5de166dab507cb26915014d
ahmedhz123 <ahmedhamzazagger123@gmail.com>
2025-11-10 22:15:44 +0200

    finally

--- config/plugins.js
+++ config/plugins.js
@@ -1,20 +1,16 @@
 module.exports = ({ env }) => ({
-  // 👇 Cloudinary upload provider
   upload: {
     config: {
-      provider: '@strapi/provider-upload-cloudinary',
+      provider: 'strapi-provider-upload-cloudinary',
       providerOptions: {
-        cloud_name: env('dzhcl678q'),
-        api_key: env('831287848883921'),
-        api_secret: env('wcToaj4erGImKZ8l96AgPn8_n4E'),
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
       },
-    },
-  },
-
-  // 👇 Keep your existing users-permissions config
-  'users-permissions': {
-    config: {
-      jwtSecret: env('JWT_SECRET', 'BA6/LPDxi1g37/KpZXcJNQ=='),
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
     },
   },
 });
